de3dd5be4d64d5c5fd0fc0ea3d77ba66e9894e57
Ubuntu <ubuntu@ip-172-31-49-132.us-west-2.compute.internal>
2025-02-25 22:46:21 +0000

    done

--- npu/conv_npu.py
+++ npu/conv_npu.py
@@ -9,16 +9,9 @@ def conv2d(X, W, bias):
 
     batch_size, in_channels, input_height, input_width = X.shape
     out_channels, in_channels_, filter_height, filter_width = W.shape
-    out_channels_ = bias.shape[0]
-
-    assert (
-        in_channels_ == in_channels and out_channels_ == out_channels
-    ), f"Shape mismatch. {in_channels}, {in_channels_}, {out_channels}, {out_channels_}"
 
     out_height = input_height - filter_height + 1
     out_width = input_width - filter_width + 1
-
-    assert in_channels % 128 == 0
 
     X_out = nl.ndarray(
         shape=(batch_size, out_channels, out_height, out_width),
@@ -30,7 +23,7 @@ def conv2d(X, W, bias):
     input_chan_block_size = nl.tile_size.pmax
     num_input_chan_tiles = in_channels // input_chan_block_size
 
-    output_chan_block_size = nl.tile_size.pmax
+    output_chan_block_size = input_chan_block_size
     num_output_chan_tiles = out_channels // output_chan_block_size
 
     output_height_block_size = 2
@@ -57,8 +50,8 @@ def conv2d(X, W, bias):
         dtype=bias.dtype,
         buffer=nl.sbuf,
     )
-    for j in nl.affine_range(num_output_chan_tiles):
-        bias_buf[j] = nl.load(bias[j])
+    for out_chan_tile in nl.affine_range(num_output_chan_tiles):
+        bias_buf[out_chan_tile] = nl.load(bias[out_chan_tile])
 
     W = W.reshape(
         (
@@ -82,12 +75,55 @@ def conv2d(X, W, bias):
         dtype=W.dtype,
         buffer=nl.sbuf,
     )
-    for i in nl.affine_range(num_output_chan_tiles):
-        weights[i] = nl.load(W[i])
+    for out_chan_tile in nl.affine_range(num_output_chan_tiles):
+        weights[out_chan_tile] = nl.load(W[out_chan_tile])
+
+    weights_temp = nl.ndarray(
+        (
+            filter_height,
+            filter_width,
+            num_output_chan_tiles,
+            num_input_chan_tiles,
+            nl.par_dim(output_chan_block_size),
+            input_chan_block_size,
+        ),
+        dtype=W.dtype,
+        buffer=nl.sbuf,
+    )
+    weights_transposed = nl.ndarray(
+        (
+            filter_height,
+            filter_width,
+            num_output_chan_tiles,
+            num_input_chan_tiles,
+            nl.par_dim(input_chan_block_size),
+            output_chan_block_size,
+        ),
+        dtype=W.dtype,
+        buffer=nl.sbuf,
+    )
+    for out_chan_tile in nl.affine_range(num_output_chan_tiles):
+        for in_chan_tile in nl.affine_range(num_input_chan_tiles):
+            for height_idx in nl.affine_range(filter_height):
+                for width_idx in nl.affine_range(filter_width):
+                    weights_temp[
+                        height_idx, width_idx, out_chan_tile, in_chan_tile, :, :
+                    ] = nl.copy(
+                        weights[
+                            out_chan_tile, :, in_chan_tile, :, height_idx, width_idx
+                        ]
+                    )
+                    weights_transposed[
+                        height_idx, width_idx, out_chan_tile, in_chan_tile, :, :
+                    ] = nisa.nc_transpose(
+                        weights_temp[
+                            height_idx, width_idx, out_chan_tile, in_chan_tile, :, :
+                        ]
+                    )
 
     # Loop over each batch image
-    for b in nl.affine_range(batch_size):
-        for a in nl.affine_range(num_output_height_tiles):
+    for batch_idx in nl.affine_range(batch_size):
+        for height_tile in nl.affine_range(num_output_height_tiles):
             img_sbuf = nl.ndarray(
                 (
                     num_input_chan_tiles,
@@ -98,48 +134,64 @@ def conv2d(X, W, bias):
                 dtype=X.dtype,
                 buffer=nl.sbuf,
             )
-            for i in nl.affine_range(num_input_chan_tiles):
-                img_sbuf[i] = nl.load(
+            for in_chan_tile in nl.affine_range(num_input_chan_tiles):
+                img_sbuf[in_chan_tile] = nl.load(
                     X[
-                        b,
-                        i,
+                        batch_idx,
+                        in_chan_tile,
                         :,
-                        a * tile_height : (a + 1) * tile_height + filter_height - 1,
+                        height_tile * tile_height : (height_tile + 1) * tile_height
+                        + filter_height
+                        - 1,
                         :,
                     ]
                 )
 
-            for i in nl.affine_range(num_output_chan_tiles):
+            for out_chan_tile in nl.affine_range(num_output_chan_tiles):
                 curr_output = nl.ndarray(
                     (nl.par_dim(output_chan_block_size), tile_height, out_width),
                     dtype=X.dtype,
                     buffer=nl.sbuf,
                 )
 
-                for curr_row in nl.affine_range(tile_height):
+                for height_offset in nl.affine_range(tile_height):
                     accum = nl.zeros(
                         (output_chan_block_size, out_width),
                         dtype=nl.float32,
                         buffer=nl.psum,
                     )
                     # Accumulate partial sums
-                    for fh in nl.affine_range(filter_height):
-                        for fw in nl.affine_range(filter_width):
-                            for j in nl.affine_range(num_input_chan_tiles):
+                    for filter_h in nl.affine_range(filter_height):
+                        for filter_w in nl.affine_range(filter_width):
+                            for in_chan_tile in nl.affine_range(num_input_chan_tiles):
                                 accum += nl.matmul(
-                                    weights[i, :, j, :, fh, fw],
-                                    img_sbuf[j, :, curr_row + fh, fw : fw + out_width],
-                                    transpose_x=False,
+                                    weights_transposed[
+                                        filter_h,
+                                        filter_w,
+                                        out_chan_tile,
+                                        in_chan_tile,
+                                        :,
+                                        :,
+                                    ],
+                                    img_sbuf[
+                                        in_chan_tile,
+                                        :,
+                                        height_offset + filter_h,
+                                        filter_w : filter_w + out_width,
+                                    ],
+                                    transpose_x=True,
                                 )
                     # Add bias
-                    accum = nisa.tensor_scalar(accum, nl.add, bias_buf[i])
-                    curr_output[:, curr_row, :] = nl.copy(accum)
+                    accum = nisa.tensor_scalar(accum, nl.add, bias_buf[out_chan_tile])
+                    curr_output[:, height_offset, :] = nl.copy(accum)
 
                 nl.store(
                     X_out[
-                        b,
-                        i * output_chan_block_size : (i + 1) * output_chan_block_size,
-                        a * tile_height : (a + 1) * tile_height,
+                        batch_idx,
+                        out_chan_tile
+                        * output_chan_block_size : (out_chan_tile + 1)
+                        * output_chan_block_size,
+                        height_tile * tile_height : (height_tile + 1) * tile_height,
                         :,
                     ],
                     value=curr_output[...],
